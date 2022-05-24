cf77df5e58b7c7da35cc0c59254addc543b75709
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-05-24 02:41:46 -0400

    update header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,53 @@
-import styles from './Header.module.css';
+import React from 'react';
 import { AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai';
-import { CgProfile } from 'react-icons/cg';
-import { MouseEvent } from 'react';
+
+import styles from './Header.module.css';
 
 export function Header() {
-  const handleOnClick = (e: MouseEvent<HTMLButtonElement>) => {
-    console.log(e.currentTarget.value);
+  const handleOnSubmit = (e: React.FormEvent) => {
+    alert('Not yet implemented');
+  };
+  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    alert('Not yet implemented');
   };
 
   return (
     <header className={styles.container}>
-      <img
-        src="./src/assets/colorwheel.svg"
-        alt="Colour Shop Logo"
-        className={styles.logo}
-      />
-      <h1>Colour Shop</h1>
-      <div className={styles.buttonsContainer}>
-        <button
-          className={styles.profile}
-          onClick={handleOnClick}
-          value="profile"
-        >
-          <CgProfile size={24} />
+      <div className={styles.content}>
+        <img
+          src="src/assets/Amazon_logo.svg"
+          alt="E-Commerce App Logo"
+          className={styles.logo}
+        />
+        <form className={styles.form} onSubmit={handleOnSubmit}>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search"
+            className={styles.input}
+          />
+          <button type="submit" className={styles.search} tabIndex={-1}>
+            <AiOutlineSearch size={20} />
+          </button>
+        </form>
+      </div>
+      <div className={styles.buttons}>
+        <button className={styles.signin} onClick={handleOnClick}>
+          <p>Hello, Sign in</p>
+          <p>
+            <b> Accounts & Lists</b>
+          </p>
         </button>
-        <button className={styles.cart} onClick={handleOnClick} value="cart">
+        <button className={styles.order} onClick={handleOnClick}>
+          <p>Returns</p>
+          <p>
+            <b>& Orders</b>
+          </p>
+        </button>
+        <button className={styles.cart} onClick={handleOnClick}>
           <AiOutlineShoppingCart size={24} />
+          <span className={styles.count}>0</span>
         </button>
       </div>
     </header>
