378a77affa79586f03f8bbec6f999de4b94984f0
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-05-31 04:09:13 -0400

    added authslice and userapi

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query/react';
+import { authSlice } from './features/authSlice';
 import { productApi } from './services/product';
+import { userApi } from './services/user';
 
 export const store = configureStore({
   reducer: {
-    [productApi.reducerPath]: productApi.reducer
+    [productApi.reducerPath]: productApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
+    [authSlice.name]: authSlice.reducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(productApi.middleware)
 });
 
 setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
