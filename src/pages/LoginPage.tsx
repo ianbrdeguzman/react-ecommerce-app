6d89f14e17bffa3ed49677c98ef253c4accdc88b
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-06-05 00:02:09 -0400

    added profile update feature

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Alert } from '../components/Alert';
 import { userSlice } from '../redux/features/userSlice';
-import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { useAppDispatch } from '../redux/hooks';
 import { useSignInMutation } from '../redux/services/userApi';
 import { useForm, SubmitHandler } from 'react-hook-form';
 
@@ -21,7 +21,6 @@ interface Error {
 export default function LoginPage() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const { user } = useAppSelector((state) => state.userSlice);
   const [signIn, { isLoading, isSuccess }] = useSignInMutation();
   const [error, setError] = useState<Error>();
   const {
