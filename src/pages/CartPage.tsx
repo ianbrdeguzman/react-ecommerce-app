5ff107c16235f038d7cc8f03971bdc6c429dfd54
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-06-06 22:28:38 -0400

    styled cart page

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,12 +1,20 @@
 import { Ad } from '../components/Ad';
+import { CartList } from '../components/CartList';
 import { EmptyCart } from '../components/EmptyCart';
+import { useAppSelector } from '../redux/hooks';
+
 import styles from './CartPage.module.css';
 
 export default function CartPage() {
+  const { cartItems } = useAppSelector((state) => state.cartSlice);
   return (
     <div className={styles.container}>
       <Ad />
-      <EmptyCart />
+      {cartItems.length === 0 ? (
+        <EmptyCart />
+      ) : (
+        <CartList cartItems={cartItems} />
+      )}
     </div>
   );
 }
